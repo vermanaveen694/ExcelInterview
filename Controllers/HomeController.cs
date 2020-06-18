18b4e3e2ea8f0e2a7b404ba675f869cbed7f93a4
vermanaveen694 <vermanaveen694@gmail.com>
2020-06-18 09:27:08 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using InterviewExcel.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace InterviewExcel.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateRecord")]
+        public bool UpdateRecord([FromBody]User user1)
+        {
+            // string url = "C:\\Users\\w.8\\Desktop\\InterViewTest\\InterviewExcel\\editableForm.xlsx";
+           //Path Get Excel File
+            string url = Environment.CurrentDirectory + @"\\editableForm.xlsx";
+            // Open the existing excel file
+            var workbook = new XLWorkbook(url); 
+            var worksheet = workbook.Worksheets.Worksheet(1);
+            //Update Value in columns In Excel
+            worksheet.Cell("D5").SetValue(user1.Name  + " "+ user1.Address);
+            worksheet.Cell("D6").SetValue(user1.PAN );
+            worksheet.Cell("D7").SetValue(user1.FinacialYear);
+            worksheet.Cell("A47").SetValue(user1.Place);
+            worksheet.Cell("A48").SetValue(user1.Date);
+            worksheet.Cell("A49").SetValue(user1.Designation);
+            workbook.Save();
+            return true;
+        }
+
+
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewExcel.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PAN { get; set; }
+        public string FinacialYear { get; set; }
+        public string Place { get; set; }
+        public DateTime Date { get; set; }
+        public string Designation { get; set; }
+    }
+}
